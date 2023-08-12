68a0f102e63a3a52be89406db26de546bfa32d27
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-12 12:18:39 -0700

    added button link

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Hero = ({ heading }) => {
   return (
     <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
@@ -5,7 +7,7 @@ const Hero = ({ heading }) => {
       <div className="absolute top-0 left-0 right-0 bottom-0 z-[2] bg-black/50" />
       <div className="p-5 text-white z-[2]  mt-[-10rem]">
         <h2 className="text-4xl">Schimscheimer Family Trio</h2>
-        <h3 className='pt-2 '>{heading}</h3>
+        {/* <h3 className='pt-2 '>{heading}</h3> */}
         <p className="py-5 pr-[4rem]">
           Continuing with their tradition of composing sonnets to beloved,
           albeit often fabricated relatives, the Schimscheimer Family Trio’s 3rd
@@ -13,7 +15,12 @@ const Hero = ({ heading }) => {
           reunion–-a multidimensional soundscape of sometimes delicate, other
           times maniacal moments.
         </p>
-        <button className="px-8 py-2 border">Listen to Genogram</button>
+        <Link
+          href="https://schimscheimerfamilytrio.bandcamp.com/album/genogram"
+          target="_blank"
+        >
+          <button className="px-8 py-2 border">Listen to Genogram</button>
+        </Link>
       </div>
     </div>
   );
