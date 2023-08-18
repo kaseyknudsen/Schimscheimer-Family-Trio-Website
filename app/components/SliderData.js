81e3a0ba1a8de8330bbc5c9acba2571e920909ed
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-18 15:13:58 -0700

    added images

---
+++ app/components/SliderData.js
@@ -0,0 +1,55 @@
+import ArkinFamily from "../../public/Edited-Schim-Pics/aronsonarkin2.jpg";
+import BabyKasey from "../../public/Edited-Schim-Pics/BabyKasey.jpg";
+import BabyJon from "../../public/Edited-Schim-Pics/BabyJon.jpg";
+import EllenDan from "../../public/Edited-Schim-Pics/Ellen_Dan.jpg";
+import em_truffle from "../../public/Edited-Schim-Pics/em_truffle.jpg";
+import emasBaby from "../../public/Edited-Schim-Pics/emasbaby-bbypoppa.jpg";
+import gert from "../../public/Edited-Schim-Pics/gert.jpg";
+import JonAndEmily from "../../public/Edited-Schim-Pics/JonAndEmily.jpg";
+import KaseyAndCousin from "../../public/Edited-Schim-Pics/KaseyAndCousin.jpg";
+import KaseyAndGrandma from "../../public/Edited-Schim-Pics/KaseyAndGrandma.jpg";
+import KaseysMom2 from "../../public/Edited-Schim-Pics/KaseysMom2.jpg";
+import KaseysMom from "../../public/Edited-Schim-Pics/KaseysMom.jpg";
+import KaseysParents from "../../public/Edited-Schim-Pics/KaseysParents.jpg";
+
+export const SliderData = () => [
+  {
+    image: ArkinFamily,
+  },
+  {
+    image: BabyKasey,
+  },
+  {
+    image: BabyJon,
+  },
+  {
+    image: EllenDan,
+  },
+  {
+    image: em_truffle,
+  },
+  {
+    image: emasBaby,
+  },
+  {
+    image: gert,
+  },
+  {
+    image: JonAndEmily,
+  },
+  {
+    image: KaseyAndCousin,
+  },
+  {
+    image: KaseyAndGrandma,
+  },
+  {
+    image: KaseysMom,
+  },
+  {
+    image: KaseysMom2,
+  },
+  {
+    image: KaseysParents,
+  },
+];
