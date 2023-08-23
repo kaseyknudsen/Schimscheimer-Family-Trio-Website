144cf4c6442c462cd0e8f5e0ba525a493c402b0a
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-23 09:50:52 -0700

    finished layout of about page

--- app/about/page.js
+++ app/about/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import michaelImage from "../../public/Edited-Schim-Pics/michael.jpg";
 import kaseyImage from "../../public/Edited-Schim-Pics/kasey.jpg";
 import jonImage from "../../public/Edited-Schim-Pics/jonarkinpromo.jpg";
+import trio from "../../public/Edited-Schim-Pics/trio.jpeg";
 export const metadata = {
   title: "About The Schimscheimer Family Trio",
   description: "Everyone's Favorite Fake Family Band",
@@ -9,13 +10,16 @@ export const metadata = {
 
 const aboutPage = () => {
   return (
-    <div className="flex relative custom-img2 bg-fixed bg-cover h-full bg-center flex-col pt-[25%] pl-4 text-">
+    <div className="flex relative custom-img2 bg-fixed bg-center flex-col pt-[25%] pl-4">
       <div className="absolute top-0 left-0 right-0 bottom-0 z-[2] bg-black/60" />
 
       <div className="z-10 text-white">
         <div className="mb-20 flex items-center justify-center flex-col px-10 pr-[4rem]">
-          <h2 className="text-4xl pb-4">The Schimscheimer Family Trio</h2>
-
+          <div className="border rounded">
+            <Image src={trio} width={300} height={400} alt="Michael Coleman" />
+          </div>
+          <h2 className="text-4xl pb-4 pt-5">The Schimscheimer Family Trio</h2>
+          {/* schimscheimer bio */}
           <p>
             As if from an actual collection of blood relatives, the music of the
             Schimscheimer Family Trio opens a window into an alternate world—one
@@ -27,9 +31,11 @@ const aboutPage = () => {
             note.
           </p>
 
-          {/* <div class="h-0.5 w-full bg-purple-400 lg:w-1/2 mt-3"></div> */}
+          <div class="h-0.5 w-[75%] bg-white my-8" />
         </div>
-        <div className="pl-5 pt-10">
+        {/* all bios */}
+
+        <div className="pt-10">
           <Image
             src={michaelImage}
             width={300}
@@ -37,19 +43,20 @@ const aboutPage = () => {
             alt="Michael Coleman"
           />
         </div>
-        <div className="p-5 text-white font-medium">
+        <div className="text-white font-medium">
           {/* michael */}
-          <div>
-            <h2 className="text-3xl">Michael Coleman</h2>
-            <p className="py-5 pr-[4rem] ">
-              Michael Coleman is a pianist/composer living in Brooklyn, NY. He
-              can be found leading his own groups (Michael Rocketship, Enjoyer,
-              Beep!) or playing, recording and touring in a wide variety of
-              other people's projects (Ben Goldberg, Chris Cohen, tUnE-yArDs,
-              Sam Evian). When not playing keyboard instruments, Michael works
-              as a recording engineer at Figure 8 Recording.
-            </p>
-          </div>
+
+          <h2 className="text-3xl pt-5">Michael Coleman</h2>
+          <p className="py-5 pr-[4rem] ">
+            Michael Coleman is a pianist/composer living in Brooklyn, NY. He can
+            be found leading his own groups (Michael Rocketship, Enjoyer, Beep!)
+            or playing, recording and touring in a wide variety of other
+            people's projects (Ben Goldberg, Chris Cohen, tUnE-yArDs, Sam
+            Evian). When not playing keyboard instruments, Michael works as a
+            recording engineer at Figure 8 Recording.
+          </p>
+          <div class="h-0.5 w-[75%] bg-white my-8" />
+
           {/* kasey */}
           <div className="pt-10">
             <Image
@@ -58,7 +65,8 @@ const aboutPage = () => {
               height={400}
               alt="Kasey Knudsen"
             />
-            <h2 className="text-3xl">Kasey Knudsen</h2>
+
+            <h2 className="text-3xl pt-5">Kasey Knudsen</h2>
             <p className="py-5 pr-[4rem] ">
               Kasey Knudsen, a graduate of Berklee College of Music, has been a
               vital part of the bay area music community since 2002. In addition
@@ -70,6 +78,7 @@ const aboutPage = () => {
               Lafayette Jazz Workshop, Sonoma State University, SFJazz, The
               Monterey Jazz Festival Education Program, and many more.
             </p>
+            <div class="h-0.5 w-[75%] bg-white my-8" />
           </div>
           {/* jon */}
           <div className="pt-10">
@@ -79,7 +88,7 @@ const aboutPage = () => {
               height={400}
               alt="Kasey Knudsen"
             />
-            <h2 className="text-3xl">Jon Arkin</h2>
+            <h2 className="text-3xl pt-5">Jon Arkin</h2>
             <p className="py-5 pr-[4rem] ">
               A graduate of the University of Miami, Jon Arkin is an in-demand
               freelance drummer with many notable local, national and
