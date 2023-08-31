336bc019ec2f206cb69284cdac274b5b4b8fa6f4
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-30 22:46:28 -0700

    finally fixed spacing

--- app/listen/page.js
+++ app/listen/page.js
@@ -9,10 +9,13 @@ const page = () => {
   return (
     <div className="custom-img5 bg-fixed bg-center min-h-screen relative top-0 left-0 right-0 bottom-0 z-[-1]">
       <div className="absolute top-0 left-0 right-0 bottom-0 z-[2] bg-black/50" />
-      
-      <div className="h-full flex flex-col gap-10 items-center justify-center z-10 relative text-white text-3xl">
-        <div className="mt-60">
-        <h1 className="text-white text-5xl mb-40 text-center">Listen to The Schimscheimers</h1>
+
+      <div className="h-full flex flex-col ml-10 z-10 relative text-white text-3xl">
+        <p className="text-white text-6xl mt-60 z-10 relative">
+          Listen to the Schimscheimers
+        </p>
+        <div className="mt-5 flex flex-col gap-20">
+          <div class="h-0.5 w-[83%] bg-white my-8" />
           {videoData.map((video, idx) => {
             return <YouTubeEmbed videoId={video.id} title={video.title} />;
           })}
