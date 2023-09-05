f85c394edb424a299bda1adc416e957ae9640d78
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-09-05 12:21:06 -0700

    fixed opacity on shows page

--- app/shows/page.js
+++ app/shows/page.js
@@ -8,13 +8,14 @@ export const metadata = {
 const page = () => {
   return (
     <div className="custom-img6 bg-fixed bg-center min-h-screen z-[-1]">
+      <div className="absolute top-0 left-0 right-0 bottom-0 z-[1] bg-black/50" />
       <div className="h-full flex flex-col ml-10 mr-[4em]">
         <div className="flex flex-col gap-5">
           <p className="text-white text-6xl mt-60 z-10 text-center fadeIn4s">
             Shows
           </p>
           <div class="h-0.5 w-[100%] bg-white my-8" />
-          <div className="flex flex-col gap-5 fadeIn5s">
+          <div className="flex flex-col gap-5 fadeIn5s z-10">
             {showData.map((show, idx) => {
               return (
                 <ShowCard
@@ -31,7 +32,7 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div className="absolute top-0 left-0 right-0 bottom-0 z-[2] bg-black/50" />
+      
     </div>
   );
 };
