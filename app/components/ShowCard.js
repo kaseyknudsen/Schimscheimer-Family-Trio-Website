7a9e32422720c9da23435489c68b618d3e146f90
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-09-04 18:45:16 -0700

    finished making show cards responsive

--- app/components/ShowCard.js
+++ app/components/ShowCard.js
@@ -1,17 +1,17 @@
 const ShowCard = ({ date, venue, location, time, website, link }) => {
   return (
-    <div className="h-38 bg-white rounded flex flex-row justify-between text-black font-semibold z-20">
-      <div className="p-4 flex flex-col gap-2 items-start">
-        <p className="text-md">{date}</p>
-        <p className="text-xl">{venue}</p>
-        <p className="text-md">{location}</p>
-        <p className="text-md">{time}</p>
+    <div className="h-20 show-card md:h-40 bg-white rounded flex flex-row sm:justify-between text-black font-semibold z-20 overflow-hidden">
+      <div className="p-3 flex flex-col md:gap-2 items-start">
+        <p className="text-xs md:text-md">{date}</p>
+        <p className="text-xs md:text-xl">{venue}</p>
+        <p className="text-xs md:text-lg">{location}</p>
+        <p className="text-xs md:text-lg">{time}</p>
       </div>
-      <a href={link} className="block hover:no-underline">
-        <div className="h-[4em] w-[15em] bg-black relative flex flex-col justify-center mt-[4em] mr-5">
-          <p className=" text-white/80 text-center">{website}</p>
-        </div>
-      </a>
+      <div className=" hidden sm:flex flex-col justify-center">
+        <a href={link} className="block hover:no-underline">
+          <button className="bg-black text-white pt-3 pb-3 px-5 md:pt-5 md:pb-5 md:px-10 mr-5">{website}</button>
+        </a>
+      </div>
     </div>
   );
 };
