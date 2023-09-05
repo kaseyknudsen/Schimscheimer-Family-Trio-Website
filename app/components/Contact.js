d8346c45e95e593ffb02cc53019aab9126dbe82b
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-09-04 20:09:54 -0700

    added conditional rendering

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -56,9 +56,77 @@ const Contact = () => {
   return (
     <div className="sm:min-w-[300px] md:min-w-[768px] lg:max-w-[1240px] text-white form-container">
       <div className="flex justify-center">
-        <h1 className=" text-white text-5xl mb-10 fadeIn3s">Get In Touch</h1>
+        <h1 className=" text-white text-5xl mb-10 fadeIn3s">
+          {!formSubmitted ? "Get In Touch" : "Form Submitted!"}
+        </h1>
       </div>
-      <form className="max-w-[800px] m-auto" onSubmit={createUser}>
+      {!formSubmitted ? (
+        <form className="max-w-[800px] m-auto" onSubmit={createUser}>
+          <div className="grid sm:grid-cols-2 gap-3 fadeIn4s">
+            <input
+              type="text"
+              className="p-3 text-black"
+              placeholder="Name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+            {formErrors.name && (
+              <div className="text-red-500">{formErrors.name}</div>
+            )}
+            <input
+              type="text"
+              className="p-3 text-black"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            {formErrors.email && (
+              <div className="text-red-500">{formErrors.email}</div>
+            )}
+          </div>
+          <div>
+            <textarea
+              className="w-full my-4 text-black fadeIn5s"
+              cols="30"
+              rows="10"
+              placeholder="Enter Comments Here"
+              onChange={(e) => setComments(e.target.value)}
+              value={comments}
+            ></textarea>
+          </div>
+          <div className="text-white mt-5 mb-5 text-xl">
+            {formSubmitted ? "Form Submitted!" : ""}
+          </div>
+
+          <p className="mb-2 fadeIn6s">
+            {!formSubmitted
+              ? "Would you like to be added to our mailing list?"
+              : null}
+          </p>
+          <label htmlFor="mailing-list-yes fadeIn6s" className="mr-2">
+            {!formSubmitted ? "Yes" : null}
+          </label>
+          {!formSubmitted ? (
+            <input
+              type="checkbox"
+              id="mailing-list-yes"
+              name="mailing-list"
+              onChange={(e) => setMailingListYes(e.target.checked)}
+              checked={mailingListYes}
+              className="mr-5 fadeIn7s"
+            />
+          ) : null}
+
+          <div className="mt-5 bg-white w-1/4 h-10 flex border-r-2 justify-center fadeIn8s">
+            {!formSubmitted ? (
+              <button type="submit" className="text-black font-semibold">
+                Submit
+              </button>
+            ) : null}
+          </div>
+        </form>
+      ) : null}
+      {/* <form className="max-w-[800px] m-auto" onSubmit={createUser}>
         <div className="grid sm:grid-cols-2 gap-3 fadeIn4s">
           <input
             type="text"
@@ -123,7 +191,7 @@ const Contact = () => {
             null
           )}
         </div>
-      </form>
+      </form> */}
     </div>
   );
 };
