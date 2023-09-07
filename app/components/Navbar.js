cd1d19e5a3e84be45dc9e62ff6ade0fd120157af
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-09-07 00:24:48 -0700

    took out fixed class on navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed left-0 top-0 w-full z-20 ease-in duration-300 text-white text-xl">
+    <div className="relative bg-black left-0 top-0 w-full z-20 ease-in duration-300 text-white text-xl">
       <div className="pl-5 m-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-5xl p-2 hover:scale-125 transition duration-700 ease-in-out">
